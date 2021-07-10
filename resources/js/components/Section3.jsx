46ea4681db514ca299cbe6a68e30918dcfd96669
Nazam <nazam98-be@email.com>
2021-07-10 18:25:24 +0200

    ajout avatar about

--- resources/js/components/Section3.jsx
+++ resources/js/components/Section3.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import Bg from '../images/bg.webp';
+// import Bg from '../images/bg.webp';
+import Bg from '../images/about2_new.jpg';
 import Square from '../images/square-dotted.png';
 import Skills from './Skills';
 const Section3 = () => {
@@ -11,7 +12,7 @@ const Section3 = () => {
 
                 <div className="row align-items-center">
                     <div className="offset-lg-2 col-lg-4 my-5">
-                        <span>About</span>  
+                        <span>About</span>
                         <h2>About me</h2>
                         <p className='my-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor reiciendis blablablablablabalblalbalblalbalbl</p>
                         {window.screen.width >= 800 && <button className="btn rounded-0 px-4 py-2" onClick={() => { "window.location.href= 'MUHAMMAD_Nazam_CV.docx';" }}><i className="fa fa-download"></i> Download CV</button>}
@@ -19,7 +20,7 @@ const Section3 = () => {
 
                     <div className="col-lg-5">
                         {window.screen.width > 1000 && <img src={Square} alt="" id='dotted' />}
-                        <img src={Bg} alt="" className='img-fluid w-75' />
+                        <img src={Bg} alt="" className='img-fluid w-100 about-img' />
                     </div>
                     {window.screen.width < 800 && <button className="btn rounded-0 px-4 py-2 my-5" onClick={() => { "window.location.href= 'MUHAMMAD_Nazam_CV.docx';" }}><i className="fa fa-download"></i> Download CV</button>}
                 </div>
